ec8b0e7db60e447cd5fdedd1ff74a1819bd5d281
roxiefoxie <ulve_jente@hotmail.com>
2023-01-24 13:50:24 +0100

    GetMyAge

---
+++ script.js
@@ -0,0 +1,13 @@
+function getAge(yearOfBirth){
+    let todaysDate = new Date(); 
+    let year = todaysDate.getFullYear(); 
+    let age = year - yearOfBirth;
+    return age;
+}
+
+function generate(){
+    let name = document.getElementById('name').value;
+    let yearOfBirth = parseInt(document.getElementById('yearOfBirth').value);
+    let age = getAge(yearOfBirth);
+    document.getElementById('answer').innerHTML = `Hei ${name}, du blir ${age} år i år!`;
+}
